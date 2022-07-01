5f708f178711fea390c6b53494f0b26631f0cb24
Cristopher Moreno <cristpher.moreno@colibid.com>
2022-06-30 21:35:27 -0400

    #first release

--- pages/lobby.js
+++ pages/lobby.js
@@ -4,21 +4,18 @@ import Navbar from "../components/Navbar"
 import CalendarElement from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 
-
 export default function Lobby({ data }) {
 
     return (
         <div>
             <Navbar />
             <LobbyHeader />
-
             <div class="grid grid-cols-2 gap-4">
                 <div>
                     {data.data.map(e => { return (<CardElement data={e} />) })}
                 </div>
                 <CalendarElement />
             </div>
-
         </div>
     )
 }
